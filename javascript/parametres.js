892feddf652e720edc3d32757d18cff8c7509427
Sylverscal <sccd@sylverscal.net>
2026-09-01 19:38:13 +0200

    Création logiciel Menu

--- javascript/parametres.js
+++ javascript/parametres.js
@@ -7,5 +7,5 @@
  * Chemin vers les gestionnaires de requêtes Ajax
  * @type String
  */
-var g_chemin_ajax = 'https://mklanj:8890/ajax/';
-//var g_chemin_ajax = 'http://mklang.sylverscal.net/ajax/';
+var g_chemin_ajax = 'https://menu:8890/ajax/';
+//var g_chemin_ajax = 'https://menu.sylverscal.net/ajax/';
